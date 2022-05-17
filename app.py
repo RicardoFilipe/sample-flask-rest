b99730975524a015b043829db222a9175a1b6a75
Ricardo Filipe <ricardo.angelo.filipe@gmail.com>
2022-05-17 15:32:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,16 @@ from flask import render_template
 
 app = Flask(__name__)
 
+in_memory_datastore = {
+   "COBOL" : {"name": "COBOL", "publication_year": 1960, "contribution": "record data"},
+   "ALGOL" : {"name": "ALGOL", "publication_year": 1958, "contribution": "scoping and nested functions"},
+   "APL" : {"name": "APL", "publication_year": 1962, "contribution": "array processing"},
+}
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+@app.route('/programming_languages/<programming_language_name>')
+def get_programming_language(programming_language_name):
+   return in_memory_datastore[programming_language_name]
